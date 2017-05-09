87b57c68426e9bf6e838c188c94db794c0d6ae53
Wojcik94 <wojcik94@interia.pl>
2017-05-09 22:25:53 +0200

    czas nad guzikiem

--- app/src/main/java/com/example/wojcik94/kajaki/NowaGrupa.java
+++ app/src/main/java/com/example/wojcik94/kajaki/NowaGrupa.java
@@ -4,17 +4,31 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class NowaGrupa extends AppCompatActivity {
+
+    protected int hour, minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nowa_grupa);
+
+        Calendar c = Calendar.getInstance();
+        hour = c.get(Calendar.HOUR);
+        minute = c.get(Calendar.MINUTE);
     }
 
     public void setTime(View view){
         Intent intent = new Intent(this, SetTime.class);
         startActivity(intent);
+    }
+
+    protected void onResume(){
+        final TextView godzina = (TextView) findViewById(R.id.godzina) ;
+        godzina.setText(hour+":"+minute);
     }
 }
--- app/src/main/java/com/example/wojcik94/kajaki/SetTime.java
+++ app/src/main/java/com/example/wojcik94/kajaki/SetTime.java
@@ -2,12 +2,18 @@ package com.example.wojcik94.kajaki;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class SetTime extends AppCompatActivity {
+public class SetTime extends NowaGrupa {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_time);
+
+        TimePicker timePicker = (TimePicker) findViewById(R.id.time_picker);
+
+        timePicker.setIs24HourView(true);
     }
 }
