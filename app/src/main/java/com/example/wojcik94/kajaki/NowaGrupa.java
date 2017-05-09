36f0a8addd63448a289be4cdbe638ba27b943d7f
Wojcik94 <wojcik94@interia.pl>
2017-05-10 00:13:05 +0200

    dzialajace ustawianie czasu

--- app/src/main/java/com/example/wojcik94/kajaki/NowaGrupa.java
+++ app/src/main/java/com/example/wojcik94/kajaki/NowaGrupa.java
@@ -1,16 +1,22 @@
 package com.example.wojcik94.kajaki;
 
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class NowaGrupa extends AppCompatActivity {
 
     protected int hour, minute;
+    private int telefon, liczba_kajakow, stan;
+    TextView godzina;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +24,34 @@ public class NowaGrupa extends AppCompatActivity {
         setContentView(R.layout.activity_nowa_grupa);
 
         Calendar c = Calendar.getInstance();
-        hour = c.get(Calendar.HOUR_OF_DAY);
-        minute = c.get(Calendar.MINUTE);
+        this.hour = c.get(Calendar.HOUR_OF_DAY);
+        this.minute = c.get(Calendar.MINUTE);
+
+        godzina = (TextView) findViewById(R.id.godzina);
+
+        godzina.setText(hour+":"+minute);
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
     }
 
     public void setTime(View view){
-        Intent intent = new Intent(this, SetTime.class);
-        startActivity(intent);
+        new TimePickerDialog(NowaGrupa.this, onTimeSetListener, hour, minute, true).show();
     }
 
-    public void onResume(){
-        super.onResume();
-        final TextView godzina = (TextView) findViewById(R.id.godzina) ;
-        godzina.setText(hour+":"+minute);
-    }
+    TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int sminute) {
+            hour=hourOfDay;
+            minute=sminute;
+
+            godzina.setText(hour+":"+minute);
+        }
+    };
+
 }
